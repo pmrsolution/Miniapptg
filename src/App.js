1d4bed57b68c4e20f4a3285e218ff3ada048a6b3
pmrsolution <p.marasov@yandex.ru>
2025-07-17 11:42:53 +0300

    fix: корневой компонент .app и структура как в старой версии, импорт App.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './lib/queryClient';
-import WebAppGuard from './components/WebAppGuard';
+import './App.css';
 import Layout from './components/Layout';
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <WebAppGuard>
-        <Layout />
-      </WebAppGuard>
-    </QueryClientProvider>
+    <div className="app">
+      <Layout />
+    </div>
   );
 } 
