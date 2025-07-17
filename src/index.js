e28401d990bca0146a79c0fd4962ebabbb9c8c2e
pmrsolution <p.marasov@yandex.ru>
2025-07-17 15:47:34 +0300

    style: restore old layout 1:1, legacy.css, цвета, размеры, глобальные переменные

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+import './styles/legacy.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
