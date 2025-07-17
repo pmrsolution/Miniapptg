6ae27b31638b34b6d5d89d01eb11901c8f45077f
pmrsolution <p.marasov@yandex.ru>
2025-07-17 16:32:59 +0300

    fix: добавлен QueryClientProvider для поддержки react-query, современная архитектура

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.scss';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import './styles/legacy.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/queryClient';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
