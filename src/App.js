c01967a2048336da14cc28bc145a5d64561ab62a
pmrsolution <p.marasov@yandex.ru>
2025-07-24 15:25:04 +0300

    debug: lime-bar sentinel + Sidebar для pinpoint drill

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@ import React from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import WebAppGuard from './components/WebAppGuard';
 import { ChatProvider } from './context/ChatContext';
+import Sidebar from './components/Sidebar';
 
 export default function App() {
   return (
     <ThemeProvider>
       <WebAppGuard>
         <ChatProvider>
-          <div
-            style={{
-              position: 'fixed',
-              inset: 0,
-              zIndex: 9999,
-              background: 'turquoise',
-              color: 'black',
-              fontSize: 24,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            LEVEL-1-SKELETON
-          </div>
+          <div style={{
+            position: 'fixed',
+            inset: 0,
+            height: '6px',
+            background: 'lime',
+            zIndex: 1000
+          }} />
+          <Sidebar />
         </ChatProvider>
       </WebAppGuard>
     </ThemeProvider>
