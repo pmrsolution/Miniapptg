47ba21bddb29fa2c4078846282278501db1962b2
pmrsolution <p.marasov@yandex.ru>
2025-07-24 14:07:51 +0300

    debug: JSON.stringify sweep и hook-by-hook probe в ChatProvider

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import useChats from '../hooks/useChats';
+import { useUser } from './UserContext';
 
 export function ChatProvider({ children }) {
-  console.log('[ChatProvider] entry');
-  return (
-    <>
-      <div id="chat-provider-active" style={{position:'fixed',top:0,right:0,width:1,height:1,background:'red'}} />
-      {children}
-    </>
-  );
+  console.log('[A] useUser pre');
+  const u = useUser();
+  console.log('[A] useUser ok');
+  console.log('[B] useChats pre');
+  const c = useChats(u?.chat_id);
+  console.log('[B] useChats ok');
+  try {
+    JSON.stringify([u, c]);
+  } catch (e) {
+    console.error('[JSON leak]', e);
+    document.body.innerHTML = `<pre style='color:red'>${e}</pre>`;
+  }
+  return <>{children}</>;
 }
 
 export function useChatContext() {
