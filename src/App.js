75ee1fc600fea729720a098e20872ceca63661ab
pmrsolution <p.marasov@yandex.ru>
2025-07-24 15:00:35 +0300

    debug: LEVEL-1-SKELETON turquoise bar для бинарного поиска высоты/рендера

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { ThemeProvider } from './context/ThemeContext';
+import WebAppGuard from './components/WebAppGuard';
+import { ChatProvider } from './context/ChatContext';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <div
-        style={{
-          position: 'fixed',
-          inset: 0,
-          zIndex: 9999,
-          background: 'lime',
-          fontSize: 24,
-          color: 'black',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        LIME TEST 100vh
-      </div>
-      {/* <WebAppGuard><ChatProvider>... etc.</ChatProvider></WebAppGuard> */}
+      <WebAppGuard>
+        <ChatProvider>
+          <div
+            style={{
+              position: 'fixed',
+              inset: 0,
+              zIndex: 9999,
+              background: 'turquoise',
+              color: 'black',
+              fontSize: 24,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            LEVEL-1-SKELETON
+          </div>
+        </ChatProvider>
+      </WebAppGuard>
     </ThemeProvider>
   );
 } 
